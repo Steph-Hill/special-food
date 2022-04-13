e6725d16cbceb01c03d960dec55d434a5ca54898
Steph-Hill <stephen_hillion@outlook.fr>
2022-04-13 10:26:33 -0400

    first commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="w-24 flex flex-col items-center bg-gray-900 py-4">
+    <div class="text-lg font-semibold text-white">Le Festin</div>
+    <!-- nav content -->
+    <ul class="mt-2 text-gray-300 font-semibold">
+      <li class="mt-3 t">
+        <a href="#" class="flex flex-col items-center text-sm capitalize">
+          <svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+            <path
+              d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9
+                17v2H5v-2h4M21 3h-8v6h8V3M11 3H3v10h8V3m10
+                8h-8v10h8V11m-10 4H3v6h8v-6z"
+            ></path>
+          </svg>
+          <span>pizza</span>
+        </a>
+      </li>
+      <li class="mt-3 t">
+        <a href="#" class="flex flex-col items-center text-sm capitalize">
+          <svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+            <path
+              d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9
+                17v2H5v-2h4M21 3h-8v6h8V3M11 3H3v10h8V3m10
+                8h-8v10h8V11m-10 4H3v6h8v-6z"
+            ></path>
+          </svg>
+          <span>Drink</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
